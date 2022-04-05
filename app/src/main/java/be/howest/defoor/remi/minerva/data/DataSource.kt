33a623da7f01dddfdf0423b79cf23b2a4bd5ae23
Rémi Defoor <remi.defoor@student.howest.be>
2022-04-05 12:04:52 +0200

    create data source class

---
+++ app/src/main/java/be/howest/defoor/remi/minerva/data/DataSource.kt
@@ -0,0 +1,9 @@
+package be.howest.defoor.remi.minerva.data
+
+import be.howest.defoor.remi.minerva.model.Book
+
+class DataSource {
+    fun loadBooks(): List<Book> {
+        return emptyList()
+    }
+}
