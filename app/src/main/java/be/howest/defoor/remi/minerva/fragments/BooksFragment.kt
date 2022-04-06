13d99561f29df6b2a6f55da12c07b9c0765cd83f
Rémi Defoor <remi.defoor@student.howest.be>
2022-04-06 11:41:47 +0200

    implement basic book fragment

--- app/src/main/java/be/howest/defoor/remi/minerva/fragments/BooksFragment.kt
+++ app/src/main/java/be/howest/defoor/remi/minerva/fragments/BooksFragment.kt
@@ -1,72 +1,57 @@
 package be.howest.defoor.remi.minerva.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import be.howest.defoor.remi.minerva.R
-import be.howest.defoor.remi.minerva.adapter.BookAdapter
-import be.howest.defoor.remi.minerva.data.DataSource
-import be.howest.defoor.remi.minerva.model.Book
+import be.howest.defoor.remi.minerva.databinding.FragmentBooksBinding
+import be.howest.defoor.remi.minerva.model.BookViewModel
+import be.howest.defoor.remi.minerva.model.LogInViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BooksFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BooksFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-        TODO("boilerplate code")
-//        val books: List<Book> = DataSource().loadBooks()
-
-//        val recyclerView: RecyclerView = findViewById<RecyclerView>(R.id.books_recycler_view) // TODO refactor
-//        recyclerView.layoutManager = LinearLayoutManager(this)
-//        recyclerView.setHasFixedSize(true)
-//        recyclerView.adapter = BookAdapter(this, books)
-    }
+    private var binding: FragmentBooksBinding? = null
+    private val sharedViewModel: BookViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_books, container, false)
+        TODO("boilerplate code")
+//        val books: List<Book> = DataSource().loadBooks()
+//        val recyclerView: RecyclerView = findViewById<RecyclerView>(R.id.books_recycler_view)
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+//        recyclerView.setHasFixedSize(true)
+//        recyclerView.adapter = BookAdapter(this, books)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BooksFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BooksFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            viewModel = sharedViewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
     }
+
+    fun navigateToNotes() {
+        val action = BooksFragmentDirections.actionBooksFragmentToNotesFragment()
+        findNavController().navigate(action)
+    }
+
+    fun filterBooks() {
+        TODO("implement")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
 }
