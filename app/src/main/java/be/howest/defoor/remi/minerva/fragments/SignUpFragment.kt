fe94c4fa927b21107d5ca15f9c971940917baada
Rémi Defoor <remi.defoor@student.howest.be>
2022-04-07 14:50:19 +0200

    implement base sign up fragment

--- app/src/main/java/be/howest/defoor/remi/minerva/fragments/SignUpFragment.kt
+++ app/src/main/java/be/howest/defoor/remi/minerva/fragments/SignUpFragment.kt
@@ -5,18 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import be.howest.defoor.remi.minerva.R
+import be.howest.defoor.remi.minerva.databinding.FragmentSignUpBinding
 import be.howest.defoor.remi.minerva.model.view_models.SignUpViewModel
 
 class SignUpFragment : Fragment() {
 
-    private lateinit var viewModel: SignUpViewModel
+    private var binding: FragmentSignUpBinding? = null
+    private val viewModel: SignUpViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        val fragmentBinding: FragmentSignUpBinding = FragmentSignUpBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            model = viewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 
 }
