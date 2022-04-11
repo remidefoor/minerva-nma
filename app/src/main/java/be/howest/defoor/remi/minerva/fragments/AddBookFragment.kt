e1f4ea12375f4dfa45a94355988132f83e0e7001
Rémi Defoor <remi.defoor@student.howest.be>
2022-04-11 16:44:05 +0200

    create add book fragment

---
+++ app/src/main/java/be/howest/defoor/remi/minerva/fragments/AddBookFragment.kt
@@ -0,0 +1,38 @@
+package be.howest.defoor.remi.minerva.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import be.howest.defoor.remi.minerva.databinding.FragmentAddBookBinding
+import be.howest.defoor.remi.minerva.model.view_models.AddBookViewModel
+
+class AddBookFragment : Fragment() {
+
+    private var binding: FragmentAddBookBinding? = null
+    private val viewModel: AddBookViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding: FragmentAddBookBinding = FragmentAddBookBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
