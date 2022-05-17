838b725ec42e70f5e8699d8de1c68956af22a267
Rémi Defoor <remi.defoor@student.howest.be>
2022-05-17 11:12:33 +0200

    add barcode scanner

--- app/src/main/java/be/howest/defoor/remi/minerva/fragments/AddBookFragment.kt
+++ app/src/main/java/be/howest/defoor/remi/minerva/fragments/AddBookFragment.kt
@@ -2,11 +2,13 @@ package be.howest.defoor.remi.minerva.fragments
 
 import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import androidx.activity.result.ActivityResultLauncher
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
@@ -14,12 +16,22 @@ import be.howest.defoor.remi.minerva.MinervaApplication
 import be.howest.defoor.remi.minerva.databinding.FragmentAddBookBinding
 import be.howest.defoor.remi.minerva.model.view_models.AddBookViewModel
 import be.howest.defoor.remi.minerva.model.view_models.AddBookViewModelFactory
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
 
 class AddBookFragment : Fragment() {
 
     private var binding: FragmentAddBookBinding? = null
     private val viewModel: AddBookViewModel by activityViewModels {
         AddBookViewModelFactory((activity?.application as MinervaApplication).userRepository)
+    }
+
+    private val scanner: ActivityResultLauncher<ScanOptions> = registerForActivityResult(ScanContract()) { result ->
+        if (result.contents == null) {
+            // TODO display error message
+        } else {
+            viewModel.setIsbn(result.contents)
+        }
     }
 
     override fun onCreateView(
@@ -32,6 +44,16 @@ class AddBookFragment : Fragment() {
         fragmentBinding.fragment = this
         binding = fragmentBinding
         return fragmentBinding.root
+    }
+
+    fun scanBook() {
+        val scannerOptions = ScanOptions()
+        scannerOptions.setDesiredBarcodeFormats(ScanOptions.EAN_13)
+        scannerOptions.setOrientationLocked(false)
+        scannerOptions.setPrompt("Scan an ISBN")
+        scannerOptions.setBeepEnabled(false)
+
+        scanner.launch(scannerOptions)
     }
 
     fun addBook() {
